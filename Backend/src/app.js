0f4f0e3aa3fa0eaded2ddb0051db45ca9d4531ba
Iti Karmakar <itikarmakar2004@gmail.com>
2026-07-03 20:23:49 +0530

    Add start script for deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,9 +4,20 @@ const app = express();
 
 const cors = require("cors");
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL,
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
