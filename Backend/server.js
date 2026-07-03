54256eae1bc0b73348ba3600d7fb014d12084220
Iti Karmakar <itikarmakar2004@gmail.com>
2026-07-03 20:32:14 +0530

    Fix server port for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const app = require('./src/app');
-const connectDB = require('./src/config/db');
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 connectDB();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
-
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
